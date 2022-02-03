32287108f7b57db5954b7bd98674313d43aeb382
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-02-03 12:23:54 +0100

    created MainContainer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <header-container />
+    <main-container />
   </div>
 </template>
 
 <script>
 import HeaderContainer from './components/HeaderContainer.vue'
+import MainContainer from './components/MainContainer.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderContainer,
+    MainContainer,
     
   },
   data() {
